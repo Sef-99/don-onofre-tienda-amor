530ec0fb48451ae0fa7e14c395d7aa6e4b107f10
Sef-99 <159728978+Sef-99@users.noreply.github.com>
2024-02-12 16:02:50 -0300

    add functionality to add products to cart

--- src/app.js
+++ src/app.js
@@ -7,7 +7,66 @@ function agregarProducto(numeroId) {
     `#precio-producto-${numeroId}`
   ).textContent;
   const precio = parseFloat(precioTexto.replace(/[^\d.]/g, "")) * 1000;
-  console.log(producto);
-  console.log(nombreProducto);
-  console.log(precio);
+
+  if (localStorage.getItem(nombreProducto) === null) {
+    localStorage.setItem(nombreProducto, precio);
+  }
+
+  cambiarBoton(producto, numeroId, "eliminar");
 }
+
+function eliminarProducto(numeroId) {
+  const producto = document.getElementById(`producto-${numeroId}`);
+  const nombreProducto = producto.querySelector(
+    `#nombre-producto-${numeroId}`
+  ).textContent;
+  localStorage.removeItem(nombreProducto);
+
+  cambiarBoton(producto, numeroId, "agregar");
+}
+
+function cambiarBoton(producto, numeroId, accion) {
+  const botonAReemplazar = producto.querySelector("button");
+  const botonNuevo = document.createElement("button");
+  botonNuevo.setAttribute("onclick", `${accion}Producto(${numeroId})`);
+  const icono = document.createElement("i");
+  icono.className = cambiarIcono(accion);
+  botonNuevo.appendChild(icono);
+  botonAReemplazar.parentNode.replaceChild(botonNuevo, botonAReemplazar);
+}
+
+function cambiarIcono(accion) {
+  const iconoMas = "fa-solid fa-plus fa-lg";
+  const iconoMenos = "fa-solid fa-minus fa-lg";
+  switch (accion) {
+    case "eliminar":
+      return iconoMenos;
+    case "agregar":
+      return iconoMas;
+  }
+}
+
+function actualizarIconos() {
+  let localStorageItems = guardarLocStorObjeto();
+  for (let i = 1; i < 10; i++) {
+    const producto = document.getElementById(`producto-${i}`);
+    const nombreProducto = producto.querySelector(
+      `#nombre-producto-${i}`
+    ).textContent;
+    if (nombreProducto in localStorageItems) {
+      cambiarBoton(producto, i, "eliminar");
+    }
+  }
+}
+
+function guardarLocStorObjeto() {
+  let localStorageItems = {};
+  for (let i = 0; i < localStorage.length; i++) {
+    const item = localStorage.key(i);
+    const precio = localStorage.getItem(item);
+    localStorageItems[item] = precio;
+  }
+  return localStorageItems;
+}
+
+actualizarIconos();
