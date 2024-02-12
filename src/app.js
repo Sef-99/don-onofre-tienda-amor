2afc6d9f32bd57796257fab5938242b78d956d0a
Sef-99 <159728978+Sef-99@users.noreply.github.com>
2024-02-12 14:01:40 -0300

    add ids to products

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 function agregarProducto(numeroId) {
   const producto = document.getElementById(`producto-${numeroId}`);
-  const nombreProducto =
-    producto.querySelector("#nombre-producto-1").textContent;
-  const precioTexto = producto.querySelector("#precio-producto-1").textContent;
+  const nombreProducto = producto.querySelector(
+    `#nombre-producto-${numeroId}`
+  ).textContent;
+  const precioTexto = producto.querySelector(
+    `#precio-producto-${numeroId}`
+  ).textContent;
   const precio = parseFloat(precioTexto.replace(/[^\d.]/g, "")) * 1000;
   console.log(producto);
   console.log(nombreProducto);
