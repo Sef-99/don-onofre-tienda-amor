e563a5c8ee7ed958e3ab72f5abc18fede6a3faaa
Sef-99 <159728978+Sef-99@users.noreply.github.com>
2024-02-14 00:58:23 -0300

    add modal to notify if payment was completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+const PATH_API = "https://staging.adamspay.com/api/v1";
+const API_KEY = "ap-174d85421e89cba68d444271";
+
 actualizarCantidades();
 actualizarCantCarrito();
 agregarEventListeners();
+
+const urlParams = new URLSearchParams(window.location.search);
+const deudaId = urlParams.get("doc_id");
+
+if (deudaId !== null) {
+  const headers = {
+    apiKey: API_KEY,
+    "Content-Type": "application/json",
+  };
+  axios
+    .get(`${PATH_API}/debts/${deudaId}`, { headers: headers })
+    .then(function (response) {
+      console.log(response);
+      const estadoPago = response.data.debt.payStatus.status;
+      const parrafoNotificacionElement = document.getElementById("infoPago");
+      switch (estadoPago) {
+        case "paid":
+          parrafoNotificacionElement.innerHTML =
+            "Su pago ha sido procesado con éxito. Gracias por su compra!";
+          break;
+        case "pending":
+          const payUrl = response.data.debt.payUrl;
+          parrafoNotificacionElement.innerHTML = `Su pago sigue pendiente. Por favor, paguelo aqui: <a href="${payUrl}" target="_blank">${payUrl}</a>`;
+          break;
+      }
+      const modalNotificacion = document.getElementById("modalNotificacion");
+      modalNotificacion.showModal();
+    });
+}
