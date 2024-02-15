ad35699cfe6838dfdc7595683f773ce1fa5bdbff
Sef-99 <159728978+Sef-99@users.noreply.github.com>
2024-02-15 12:13:10 -0300

    add login functionality

--- src/login.js
+++ src/login.js
@@ -1,7 +1,12 @@
+if (localStorage.getItem("loggedUser") !== null) {
+  window.location.href = "index.html";
+}
+
 function getInfoUsuario() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   console.log({ email, password });
+  logInUsuario(email, password);
 }
 
 function togglePasswordVisibility() {
@@ -17,5 +22,19 @@ function togglePasswordVisibility() {
     passwordInput.type = "password";
     eyeClosed.style.display = "block";
     eyeOpen.style.display = "none";
+  }
+}
+
+function logInUsuario(email, password) {
+  const listaUsuarios = localStorage.getItem("users");
+  const listaParsed = JSON.parse(listaUsuarios);
+  console.log(listaParsed);
+  if (email in listaParsed) {
+    if (password === listaParsed[email].password) {
+      console.log("xd");
+      localStorage.setItem("loggedUser", email);
+      window.location.href = "index.html";
+    }
+  } else {
   }
 }
